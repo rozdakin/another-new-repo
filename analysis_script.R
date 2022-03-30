3227450c1b7ff541a1a796ef6755c9863d4e0e62
Roz Dakin <roslyn.dakin@gmail.com>
2022-03-30 13:50:56 -0400

    plot of mpg vs. weight

--- analysis_script.R
+++ analysis_script.R
@@ -4,4 +4,12 @@ mean(mtcars$mpg)
 
 library(tidyverse)
 
-mtcars %>% filter(cyl == 6)
+p <- mtcars %>% filter(cyl == 6) %>%
+  ggplot() +
+  geom_point(aes(x = wt, y = mpg)) +
+  theme_bw()
+
+ggsave(filename = 'my_mpg_graph.pdf', plot = p, device = 'pdf', width = 5, height = 5)
+
+
+
